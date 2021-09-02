6b5f62b5f3b9a03f461c5b22a408e82cb5eff420
YorkNishi999 <york.nishi@gmail.com>
2021-09-02 14:17:29 -0500

    Freeを、Busy →Freeになるように書き換える。その後、合体Coalesceするように変更

--- assign6/tests/free.c
+++ assign6/tests/free.c
@@ -10,8 +10,8 @@ int main() {
 
    ptr[0] = Mem_Alloc(4);
    ptr[1] = Mem_Alloc(8);
-   ptr[2] = Mem_Alloc(16);
-   ptr[3] = Mem_Alloc(4);
+   // ptr[2] = Mem_Alloc(16);
+   // ptr[3] = Mem_Alloc(4);
    Mem_Dump();
    printf("koko kara free\n");
    printf("ptr[1]: %p\n", ptr[1]);
@@ -20,12 +20,16 @@ int main() {
    Mem_Dump();
 
 
-   // ptr[2] = Mem_Alloc(16);
-   // ptr[3] = Mem_Alloc(4);
+   ptr[2] = Mem_Alloc(16);
+   ptr[3] = Mem_Alloc(4);
    assert(Mem_Free(ptr[2]) == 0);
-   assert(Mem_Free(ptr[3]) == 0);
+   Mem_Dump();
 
-   assert(Mem_Free((void*)0x28ff44) == -1);
+   assert(Mem_Free(ptr[3]) == 0);
+   Mem_Dump();
+
+
+//   assert(Mem_Free((void*)0x28ff44) == -1);
 
    exit(0);
 }
