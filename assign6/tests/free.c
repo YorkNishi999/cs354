3ee602e5cf4d1482043bd7cbd8f55f4703f134b9
YorkNishi999 <york.nishi@gmail.com>
2021-09-02 11:18:49 -0500

    NULL以外のfree comp

--- assign6/tests/free.c
+++ assign6/tests/free.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include "mem.h"
+#include <stdio.h>
 
 int main() {
    assert(Mem_Init(4096,0) == 0);
@@ -9,12 +10,18 @@ int main() {
 
    ptr[0] = Mem_Alloc(4);
    ptr[1] = Mem_Alloc(8);
-   Mem_Dump();
-   assert(Mem_Free(ptr[0]) == 0);
-   assert(Mem_Free(ptr[1]) == 0);
-
    ptr[2] = Mem_Alloc(16);
    ptr[3] = Mem_Alloc(4);
+   Mem_Dump();
+   printf("koko kara free\n");
+   printf("ptr[1]: %p\n", ptr[1]);
+   //assert(Mem_Free(ptr[0]) == 0);
+   assert(Mem_Free(ptr[1]) == 0);
+   Mem_Dump();
+
+
+   // ptr[2] = Mem_Alloc(16);
+   // ptr[3] = Mem_Alloc(4);
    assert(Mem_Free(ptr[2]) == 0);
    assert(Mem_Free(ptr[3]) == 0);
 
